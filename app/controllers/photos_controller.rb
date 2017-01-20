5e56c6b99dbae1d83be9eed4410e24d740efd8a2
David Chapman <davidchappy@gmail.com>
2017-01-20 16:47:46 -0300

    Add dynamic character list to view

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,7 @@
 class PhotosController < ApplicationController
   def show
     @photo = Photo.where(["selected = ?", "true"]).first
+    @characters = Character.where("photo_id = ?", @photo.id)
     @source = @photo.url
   end
 end
